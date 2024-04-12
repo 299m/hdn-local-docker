5efe6896870a69570e571df08c8eeecad61cf545
Paul <paul@brite>
2024-04-12 19:00:09 +0800

    added bin back

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 git pull
 
+mkdir bin
+cp $(which hdnprxy) bin/
+
 # Build the docker image
 sudo docker build -t hdn-local-tunnel .
 sudo docker tag hdn-local-tunnel:latest 299m/core:hdn-local-tunnel-1.0.0
